f68c73e721d7cfe598571c54e0619beae266e75a
Rio Vernanda Saputra <riovernandvernand@gmail.com>
2026-05-20 18:35:31 +0700

    tambah halaman home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,11 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        // Jika sudah login, redirect ke dashboard
-        if (session()->get('logged_in')) {
-            return redirect()->to('/dashboard');
-        }
-
-        return view('auth/login');
+        return view('home');
     }
 }
